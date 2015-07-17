9bfb58994965b216377592b24aa77d8c185631c9
Aakansha <monstershome@gmail.com>
2015-07-17 21:04:45 +0530

    Python Data Types Challenges

---
+++ HackerRank/Python/Data Types/Tuples.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Task
+You are given an integer N in one line. The next line contains N space-separated integers. Create a tuple of those N integers. Lets call it T.
+Compute hash(T) and print it.
+
+Note hash() is one of the function in __builtins__ module.
+
+Input Format
+The first line contains N. The next line contains N space-separated integer values.
+
+Output Format
+Print the computed value. 
+'''
+N=int(raw_input())
+T=tuple(map(int,raw_input().strip().split(" ")))
+print hash(T)
+    
+    
+    
+    
