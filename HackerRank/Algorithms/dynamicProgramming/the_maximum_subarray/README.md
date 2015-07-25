6cabafc5ffcd4e7452b9c31409db3998215978fa
Aakansha <monstershome@gmail.com>
2015-07-25 18:29:48 +0530

    Optimised The Maximum Subarray problem and added readme

---
+++ HackerRank/Algorithms/dynamicProgramming/the_maximum_subarray/README.md
@@ -0,0 +1,50 @@
+#The Maximum Subarray#
+
+
+###Problem Statement###
+
+Given an array A={a<sub>1</sub>,a<sub>2</sub>,…,a<sub>N</sub>} of N elements, find the maximum possible sum of a  
+
+1.    Contiguous subarray
+2.    Non-contiguous (not necessarily contiguous) subarray.
+
+Empty subarrays/subsequences should not be considered. 
+
+###Input Format###
+
+First line of the input has an integer T. T cases follow.  
+Each test case begins with an integer N. In the next line, N integers follow representing the elements of array A.  
+
+###Constraints:###
+
+*    1≤T≤10
+*    1≤N≤10<sup>5</sup>
+*    −10<sup>4</sup>≤a<sub>i</sub>≤10<sup>4</sup>
+
+The subarray and subsequences you consider should have at least one element.
+
+###Output Format###
+
+Two, space separated, integers denoting the maximum contiguous and non-contiguous subarray. At least one integer should be selected and put into the subarrays (this may be required in cases where all elements are negative).
+
+###Sample Input###
+```
+2 
+4 
+1 2 3 4
+6
+2 -1 2 3 4 -5
+```
+###Sample Output###
+```
+10 10
+10 11
+``
+###Explanation###
+
+In the first case:
+The max sum for both contiguous and non-contiguous elements is the sum of ALL the elements (as they are all positive).
+
+In the second case:
+[2 -1 2 3 4] --> This forms the contiguous sub-array with the maximum sum.
+For the max sum of a not-necessarily-contiguous group of elements, simply add all the positive elements. 
--- HackerRank/Algorithms/dynamicProgramming/the_maximum_subarray/The_Maximum_Subarray.java
+++ HackerRank/Algorithms/dynamicProgramming/the_maximum_subarray/The_Maximum_Subarray.java
@@ -1,5 +1,6 @@
+package dynamicProgramming.the_maximum_subarray;
 
-package dynamicProgramming;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,11 +10,10 @@ import java.util.StringTokenizer;
  * @author Aakansha Doshi
  *
  */
-public class The_Maximum_Subarray
- {
+public class The_Maximum_Subarray {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+		
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int T=Integer.parseInt(br.readLine());StringTokenizer st;
 		
@@ -30,6 +30,7 @@ public class The_Maximum_Subarray
 					arr[i]=Integer.parseInt(st.nextToken());
 					
 				}
+                int lis[]=new int[n1];
 				int maxcons=Integer.MIN_VALUE,maxnoncons=0;int max=Integer.MIN_VALUE;
 				for(int i=0;i<n1;i++)
 				{
@@ -37,42 +38,21 @@ public class The_Maximum_Subarray
 						maxnoncons+=arr[i];
 					if(arr[i]>max)
 						max=arr[i];
-					
-				}
-                if(maxnoncons==0)
-                {
-                    maxnoncons=max;
-                    maxcons=max;
-                }
-                else{
-				int lis[]=new int[n1];
-				
-				for(int i=0;i<n1;i++)
-				{
-					lis[i]=arr[i];
-				}
-				for(int i=1;i<n1;i++)
-				{
-					if(lis[i]<lis[i-1]+arr[i])
-					{
-						lis[i]=lis[i-1]+arr[i];
-					}
-					
-                  //  System.out.println(lis[i]);
-				}
-				
-				for(int i=0;i<n1;i++)
-				{
-					if(lis[i]>maxcons)
+                    lis[i]=arr[i];
+                    if(i>0&&lis[i-1]>0)
+                        lis[i]+=lis[i-1];
+                    if(lis[i]>maxcons)
 					{
 						maxcons=lis[i];
 					}
-				}
-				
-			}
+					
+				}				
+			     if(maxnoncons==0)
+                     maxnoncons=max;
                 System.out.println(maxcons+" " +maxnoncons);
+            }
 		}
 
 	}
-    }
+    
 }
