17b9b2f05d716664dbfec54a5d3d79cbefdc2998
Aakansha <monstershome@gmail.com>
2015-08-01 17:33:00 +0530

    Code Optimised

--- HackerRank/Algorithms/dynamicProgramming/the_maximum_subarray/The_Maximum_Subarray.java
+++ HackerRank/Algorithms/dynamicProgramming/the_maximum_subarray/The_Maximum_Subarray.java
@@ -1,54 +1,46 @@
 package dynamicProgramming.the_maximum_subarray;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * @author Aakansha Doshi
- *
- */
 public class The_Maximum_Subarray {
 
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int T=Integer.parseInt(br.readLine());StringTokenizer st;
 		
 		String s;
 		while(T-->0)
 		{
-			int n1=Integer.parseInt(br.readLine());
+			int N=Integer.parseInt(br.readLine());
 			if((s=br.readLine())!=null)
 			{
 				st=new StringTokenizer(s);
-				int arr[]=new int[n1];
-				for(int i=0;i<n1;i++)
+				int arr[]=new int[N];
+				for(int i=0;i<N;i++)
 				{
 					arr[i]=Integer.parseInt(st.nextToken());
 					
 				}
-                int lis[]=new int[n1];
-				int maxcons=Integer.MIN_VALUE,maxnoncons=0;int max=Integer.MIN_VALUE;
-				for(int i=0;i<n1;i++)
+                int lis[]=new int[N];
+				int maxcons=arr[0],maxnoncons=arr[0]>0?arr[0]:0;
+                int max=arr[0];
+				lis[0]=arr[0];
+				for(int i=1;i<N;i++)
 				{
 					if(arr[i]>0)
-						maxnoncons+=arr[i];
-					if(arr[i]>max)
-						max=arr[i];
-                    lis[i]=arr[i];
-                    if(i>0&&lis[i-1]>0)
+						maxnoncons+=arr[i];				 
+					lis[i]=arr[i];
+					if(lis[i-1]>0)
                         lis[i]+=lis[i-1];
-                    if(lis[i]>maxcons)
-					{
-						maxcons=lis[i];
-					}
+                    maxcons=Math.max(maxcons,lis[i]);
+                    max=Math.max(max,arr[i]);
 					
 				}				
-			     if(maxnoncons==0)
-                     maxnoncons=max;
+			    if(maxnoncons==0) 
+                    maxnoncons=max;
                 System.out.println(maxcons+" " +maxnoncons);
             }
 		}
