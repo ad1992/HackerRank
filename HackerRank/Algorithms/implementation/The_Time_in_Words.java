f037b23eed0637057aa781f225a1a6fdf845634f
EvilSourcerer <32974335+EvilSourcerer@users.noreply.github.com>
2018-05-07 19:30:33 -0400

    Patched the "One Minutes Past One" error
    fixed a grammar issue which caused one incorrect solution.

--- HackerRank/Algorithms/implementation/The_Time_in_Words.java
+++ HackerRank/Algorithms/implementation/The_Time_in_Words.java
@@ -1,10 +1,4 @@
-package implementation;
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import java.util.Scanner;
 /**
  * @author Aakansha Doshi
  * 
@@ -30,44 +24,62 @@ import java.io.InputStreamReader;
  * 
  *         Display the time in words.
  */
-public class The_Time_in_Words {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		String word[]={"","one","two","three","four","five","six","seven","eight","nine","ten",
-				"eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen",
-				"nineteen","twenty","twenty one","twenty two","twenty three","twenty four","twenty five","twenty six",
-				"twenty seven","twenty eight","twenty nine"};
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		int h=Integer.parseInt(br.readLine().trim());
-		int m=Integer.parseInt(br.readLine().trim());
-		String time="";
-		if(m==0)
-		{
-			time=word[h]+" o' clock";
-		}
-        else if(m==15)
-        {
-            time="quarter past "+word[h];
-        }
-		else if(m==30)
-		{
-			time="half past "+word[h];
-		}
-		else if(m==45)
-		{
-			time="quarter to "+word[h+1];
-		}
-		else if(m<30)
-		{
-			time=word[m]+" minutes past "+word[h];
-		}
-		else
-		{
-			time=word[60-m]+" minutes to "+word[h+1];
-		}
-		System.out.println(time);
-			
-	}
+public class Solution {
 
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int h = in .nextInt();
+    int m = in .nextInt();
+    String word[] = {
+      "",
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "ten",
+      "eleven",
+      "twelve",
+      "thirteen",
+      "fourteen",
+      "fifteen",
+      "sixteen",
+      "seventeen",
+      "eighteen",
+      "nineteen",
+      "twenty",
+      "twenty one",
+      "twenty two",
+      "twenty three",
+      "twenty four",
+      "twenty five",
+      "twenty six",
+      "twenty seven",
+      "twenty eight",
+      "twenty nine"
+    };
+    String time = "";
+
+    if (m == 0) {
+      time = word[h] + " o' clock";
+    } else if (m == 15) {
+      time = "quarter past " + word[h];
+    } else if (m == 30) {
+      time = "half past " + word[h];
+    } else if (m == 45) {
+      time = "quarter to " + word[h + 1];
+    } else if (m == 1) {
+      time = word[m] + " minute past " + word[h];
+    } else if (m < 30) {
+      time = word[m] + " minutes past " + word[h];
+    } else {
+      time = word[60 - m] + " minutes to " + word[h + 1];
+    }
+    System.out.println(time);
+  }
 }
